3b7a4d87d2a6309c414d7d5b61ddafb63e345d18
周龙权 <1792712497@qq.com>
2022-11-10 08:59:21 +0800

    vue学习

--- src/plugin/autoImport.ts
+++ src/plugin/autoImport.ts
@@ -7,8 +7,7 @@ export default function () {
 		imports: [
 			'vue',
 			'vue-router',
-			'vuex',
-			'@vueuse/core'
+			// '@vueuse/core'
 		],
 		// 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
 		dts: 'src/auto-import.d.ts'
