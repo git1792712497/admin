595c5f4dd56b14806f7a943474b22fc0aa8b3fe6
周龙权 <1792712497@qq.com>
2022-12-04 14:21:14 +0800

    hover.css

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="header">
+  <el-card ref="dom" shadow="hover" style="height: 100%;" :body-style="{height:'100%'}">
+    <template #header>
 
-  </div>
+    </template>
+  </el-card>
 </template>
+<script lang="ts" setup>
+const dom = shallowRef(null)
+const obj = {
 
+}
+onMounted(() => {
+
+})
+</script>
 <style>
 
 </style>
