f97b744198afd720668e1cf9fc2f812aba9c7420
周龙权 <1792712497@qq.com>
2022-12-11 13:43:20 +0800

    prettier.config.js

--- src/views/home/Loading.vue
+++ src/views/home/Loading.vue
@@ -1,57 +1,78 @@
 <template>
   <el-table ref="multipleTableRef" :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
-    <el-table-column type="selection" width="55"/>
+    <el-table-column type="selection" width="55" />
     <el-table-column label="Date" width="120">
-      <template #default="scope">{{scope.row.date}}</template>
+      <template #default="scope">{{ scope.row.date }}</template>
     </el-table-column>
-    <el-table-column label="Name" property="name" width="120"/>
-    <el-table-column label="Address" property="address" show-overflow-tooltip/>
+    <el-table-column label="Name" property="name" width="120" />
+    <el-table-column label="Address" property="address" show-overflow-tooltip />
   </el-table>
-  <div style="margin-top: 20px;">
-    <el-button @click="toggleSelection([tableData[1], tableData[2]])">Toggle selection status of second and third rows
-    </el-button>
+  <div style="margin-top: 20px">
+    <el-button @click="toggleSelection([tableData[1], tableData[2]])">Toggle selection status of second and third rows </el-button>
     <el-button @click="toggleSelection()">Clear selection</el-button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { ElTable } from 'element-plus'
+import { ref } from 'vue';
+import { ElTable } from 'element-plus';
 
 interface User {
-  date: string
-  name: string
-  address: string
+  date: string;
+  name: string;
+  address: string;
 }
 
-const multipleTableRef = ref<InstanceType<typeof ElTable>>()
-const multipleSelection = ref<User[]>([])
+const multipleTableRef = ref<InstanceType<typeof ElTable>>();
+const multipleSelection = ref<User[]>([]);
 const toggleSelection = (rows?: User[]) => {
   if (rows) {
-    rows.forEach((row) => {
-      multipleTableRef.value!.toggleRowSelection(row, undefined)
-    })
+    rows.forEach(row => {
+      multipleTableRef.value!.toggleRowSelection(row, undefined);
+    });
   } else {
-    multipleTableRef.value!.clearSelection()
+    multipleTableRef.value!.clearSelection();
   }
-}
-const handleSelectionChange = (val: User[]) => {
-  multipleSelection.value = val
-}
+};
+const handleSelectionChange = val => {
+  multipleSelection.value = val;
+};
 
-const tableData: User[] = [{
-  date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-04', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-01', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-08', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-06', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-}, {
-  date: '2016-05-07', name: 'Tom', address: 'No. 189, Grove St, Los Angeles',
-},]
+const tableData: User[] = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+];
 </script>
--- src/views/home/echarts.vue
+++ src/views/home/echarts.vue
@@ -3,69 +3,63 @@
 </template>
 
 <script>
-import { use } from "echarts/core";
-import { CanvasRenderer } from "echarts/renderers";
-import { PieChart } from "echarts/charts";
-import {TitleComponent, TooltipComponent, LegendComponent} from "echarts/components";
-import VChart from "vue-echarts";
-import { ref, defineComponent } from "vue";
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart } from 'echarts/charts'
+import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
+import VChart from 'vue-echarts'
+import { ref, defineComponent } from 'vue'
 
-use([
-  CanvasRenderer,
-  PieChart,
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent
-]);
+use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent])
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   components: {
-    VChart
+    VChart,
   },
 
   setup: () => {
     const option = ref({
       title: {
-        text: "Traffic Sources",
-        left: "center"
+        text: 'Traffic Sources',
+        left: 'center',
       },
       tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
       },
       legend: {
-        orient: "vertical",
-        left: "left",
-        data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"]
+        orient: 'vertical',
+        left: 'left',
+        data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines'],
       },
       series: [
         {
-          name: "Traffic Sources",
-          type: "pie",
-          radius: "55%",
-          center: ["50%", "60%"],
+          name: 'Traffic Sources',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '60%'],
           data: [
-            { value: 335, name: "Direct" },
-            { value: 310, name: "Email" },
-            { value: 234, name: "Ad Networks" },
-            { value: 135, name: "Video Ads" },
-            { value: 1548, name: "Search Engines" }
+            { value: 335, name: 'Direct' },
+            { value: 310, name: 'Email' },
+            { value: 234, name: 'Ad Networks' },
+            { value: 135, name: 'Video Ads' },
+            { value: 1548, name: 'Search Engines' },
           ],
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
               shadowOffsetX: 0,
-              shadowColor: "rgba(0, 0, 0, 0.5)"
-            }
-          }
-        }
-      ]
-    });
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            },
+          },
+        },
+      ],
+    })
 
-    return { option };
-  }
-});
+    return { option }
+  },
+})
 </script>
 
 <style scoped>
